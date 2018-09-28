f6377c00971417bad3bd592c85906107cbdc128b
shubhngm92 <shubh.ngm@gmail.com>
2018-09-28 23:12:16 +0530

    updated

---
+++ clustering.R
@@ -0,0 +1,18 @@
+#clustering
+
+set.seed(1234)
+subject1 = trunc(rnorm(30,mean=60,sd=15))
+range(subject1)
+subject1
+marks = data.frame(subject1)
+head(marks)
+
+k2 = kmeans(marks,centers = 2)
+k2
+k2$size
+marks[k2$cluster==1,]
+marks[k2$cluster==2,]
+mean(marks[k2$cluster==1,])
+mean(marks[k2$clsuter==2,])
+
+km4=kmean(data)
